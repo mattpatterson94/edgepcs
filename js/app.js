1fe65e123a8b1b6e0588d2d0bb0e1e3d404825ef
Jordan Gardiner <hello@jordsta.com>
2013-12-11 19:42:19 +1000

    Angular JS implementation

--- js/app.js
+++ js/app.js
@@ -1,62 +1,138 @@
 $(document).foundation();
 
-$(document).ready(function(){
-	$("section.our-computers .computers a:first").trigger("click");
-});
-
-$("section.our-computers .computers a").click(function(){
-	var $computers = $("section.our-computers .computers").find("a");
-	var $thiscomputer = $computers.index($(this));
-
-	$("section.our-computers .computers a.active").removeClass("active");
-	$(this).addClass("active");
-
-	$.ajax({
-		url: "json/test.json"
-	}).done(function(data){
-		for (var i = 0; i < data.computers.length; i++) {
-			if (i == $thiscomputer) {
-				$("section.our-computers h4.computer-name").html(data.computers[i].title + "<span class='label'>" + data.computers[i].price + "</span>");
-
-				$("section.our-computers p.computer-desc").html(data.computers[i].desc);
-
-				if (data.computers[i].specs){
-					var display = "";
-					$.each(data.computers[i].specs, function(e, l){
-						$.each(l, function(o, t){
-							display += "<div class='row'>";
-								display += "<div class='small-2 columns'><p><strong>";
-									display += o;
-								display += "</strong></p></div>";
-								display += "<div class='small-10 columns'><p>";
-									display += t;
-								display += "</p></div>";
-							display += "</div>";
-						});
-					});
-					$("section.our-computers .computer-specs").html(display);
-				} else {
-					$("section.our-computers .computer-specs").html("<p>No specifications for this computer.</p>");
-				}
-
-				return false;
-			}
-		}
-		
+function getComputers() {
+	return $.ajax({
+		url: "json/computers.json"
 	});
+}
 
-	return false;
-});
+function getAddons() {
+	return $.ajax({
+		url: "json/addons.json"
+	});
+}
 
-$("section.our-computers a.show-specs").click(function(){
-	$(".computer-specs").stop().slideToggle();
-	if ($(this).text() == "Show specifications") {
-		$(this).text("Hide specifications");
-	} else {
-		$(this).text("Show specifications");
+function computerBuild($scope) {
+	$scope.currentcomputer = [];
+	$scope.quotelist = [];
+
+	$scope.showQuote = false;
+	$scope.itemAdded = false;
+	$scope.itemRemoved = false;
+
+	$scope.totalIncomplete = false;
+
+	$scope.totalCost = 0;
+
+	$scope.addons = [];
+
+	$scope.pages = 3;
+	$scope.currentpage = 1;
+
+	$('#quoteform').parsley();
+	
+	$scope.getInfo = function(event) {
+		var $computers = $("section.our-computers .computers").find("a");
+		var $thiscomputer = 0.00;
+
+		// Remove existing active class
+		$("section.our-computers .computers a.active").removeClass("active");
+
+		if (event) {
+			$thiscomputer = $computers.index($(event.target).parent());
+			$(event.target).parent().addClass("active");
+		} else {
+			$("section.our-computers .computers a:first").addClass("active");
+		}
+
+		$.when(getComputers()).done(function(data){
+			for (var i = 0; i < data.computers.length; i++) {
+				if (i == $thiscomputer) {
+					$scope.currentcomputer = data.computers[i];
+					$scope.$apply();
+				}
+			}
+		});
+	};
+
+	$scope.listAddons = function() {
+		$.when(getAddons()).done(function(data){
+			$scope.addons = data.addons;
+			$scope.$apply();
+		});
+	};
+
+	$scope.addComputertoQuote = function() {
+		$scope.showQuote = true;
+		$scope.itemAdded = true;
+		$scope.itemRemoved = false;
+
+		var quoteAddition = {"title": $scope.currentcomputer.title, "price": $scope.currentcomputer.price, "qty": 1};
+		$scope.quotelist.push(quoteAddition);
+	};
+
+	$scope.addAddontoQuote = function(index) {
+		$scope.showQuote = true;
+		$scope.itemAdded = true;
+		$scope.itemRemoved = false;
+
+		var quoteAddition = {"title": $scope.addons[index].title, "price": $scope.addons[index].price, "qty": 1};
+		$scope.quotelist.push(quoteAddition);
+	};
+
+	$scope.removeItem = function(index) {
+		$scope.itemAdded = false;
+		$scope.itemRemoved = true;
+
+		$scope.quotelist.splice(index, 1);
+	};
+
+	$scope.total = function() {
+		var total = 0;
+		$scope.totalIncomplete = false;
+		angular.forEach($scope.quotelist, function(item) {
+			if (!isNaN(item.price)) {
+				total += item.qty * item.price;
+			} else {
+				$scope.totalIncomplete = true;
+			}
+		});
+
+		return total;
+	};
+
+	$scope.decrement = function() {
+		if ($scope.currentpage > 1) {
+			$scope.itemAdded = false;
+			$scope.itemRemoved = false;
+
+			$scope.currentpage--;
+		}
 	}
-	return false;
-});
+
+	$scope.increment = function() {
+
+		var lessthantotal = $scope.currentpage < $scope.pages;
+		var notblank = !($scope.currentpage == 2 && $scope.total() == 0 && !$scope.totalIncomplete);
+		var validform = $('#quoteform').parsley('validate');
+		console.log(validform);
+		if (lessthantotal && notblank && validform) {
+			$scope.itemAdded = false;
+			$scope.itemRemoved = false;
+			$scope.currentpage++;
+		}
+	}
+
+	$scope.getInfo();
+	$scope.listAddons();
+
+	$scope.submit = function() {
+		var validform = $('#quoteform').parsley('validate');
+		if (validform) {
+			alert("YAY!");
+		}
+	}
+}
 
 $("section.our-computers .addons a").click(function(){
 	var item = $(this).parent().next();
@@ -68,4 +144,13 @@ $("section.our-computers .addons a").click(function(){
 	console.log(itemtext + ": " + pricetext);
 
 	return false;
+});
+
+$('#quoteform').parsley('addListener', {
+    onFieldValidate: function ( elem ) {
+        if ( !$( elem ).is( ':visible' ) ) {
+            return true;
+        }
+        return false;
+    }
 });
