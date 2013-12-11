10af9286bb34c379826ac4c82337c8e2eb68694c
Jordan Gardiner <hello@jordsta.com>
2013-12-11 21:26:23 +1000

    Added tons of exciting Gruntfile stuff!

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,11 +12,58 @@ module.exports = function(grunt) {
         },
         files: {
           'css/app.css': 'scss/app.scss'
-        }        
+        }
       }
     },
 
+    concat: {
+      dist: {
+          src: [
+              'bower_components/jquery/jquery.min.js',
+              'bower_components/foundation/js/foundation.min.js',
+              // 'js/*.js'
+              // 'js/!(app.concat|app.min).js'
+              'js/parsley.min.js',
+              'js/app.js'
+          ],
+          dest: 'build/js/app.concat.js',
+      }
+    },
+
+    uglify: {
+      options: {
+        mangle: false
+      },
+      build: {
+          src: 'build/js/app.concat.js',
+          dest: 'build/js/app.min.js'
+      }
+    },
+
+    imagemin: {
+        dynamic: {
+            files: [{
+                expand: true,
+                cwd: 'images/',
+                src: ['**/*.{png,jpg,gif}'],
+                dest: 'build/images/'
+            }]
+        }
+    },
+
     watch: {
+      options: {
+          livereload: true,
+      },
+
+      scripts: {
+          files: ['js/*.js'],
+          tasks: ['concat', 'uglify'],
+          options: {
+              spawn: false,
+          },
+      },
+
       grunt: { files: ['Gruntfile.js'] },
 
       sass: {
@@ -28,7 +75,11 @@ module.exports = function(grunt) {
 
   grunt.loadNpmTasks('grunt-sass');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
 
   grunt.registerTask('build', ['sass']);
-  grunt.registerTask('default', ['build','watch']);
+  grunt.registerTask('default', ['build','concat','uglify','imagemin','watch']);
+
 }
