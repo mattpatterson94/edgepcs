2ef91b0bc8213a87ab122fbff921d0cb97ed9742
Matt Patterson <matt.patterson94@gmail.com>
2014-03-24 01:19:28 +1000

    remove SMTP (maybe later I will add)

--- includes/send_email_contact.php
+++ includes/send_email_contact.php
@@ -102,32 +102,9 @@ function send_email($subject,$recipient,$from,$fromemail,$htmlmessage,$textmessa
 
     //Create a new PHPMailer instance
     $mail = new PHPMailer();
-    //Tell PHPMailer to use SMTP
-    $mail->isSMTP();
-    // //Enable SMTP debugging
-    // // 0 = off (for production use)
-    // // 1 = client messages
-    // // 2 = client and server messages
-    $mail->SMTPDebug = 2;
-    // //Ask for HTML-friendly debug output
-     //Set the hostname of the mail server
-    $mail->Host = 'smtp.gmail.com';
-    //Set the SMTP port number - 587 for authenticated TLS, a.k.a. RFC4409 SMTP submission
-    $mail->Port = 587;
-    //Set the encryption system to use - ssl (deprecated) or tls
-    $mail->SMTPSecure = 'tls';
-    //Whether to use SMTP authentication
-    $mail->SMTPAuth = true;
-    //Username to use for SMTP authentication - use full email address for gmail
-    $mail->Username = "haydster7@gmail.com";
-    //Password to use for SMTP authentication
-    $mail->Password = "7retsdyah77";
-    //Set who the message is to be sent from
-    $mail->setFrom($fromemail, $from);
     //Set an alternative reply-to address
     // $mail->addReplyTo('replyto@example.com', 'First Last');
     //Set who the message is to be sent to
-    // $mail->addAddress('enquiry@edgepcs.com.au', 'Edge PCs');
     $mail->addAddress('enquire@edgepcs.com.au', 'Edge PCs');
     //Set the subject line
     $mail->Subject = $subject;
