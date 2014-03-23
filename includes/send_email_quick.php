1cbefac063af23f6f09775b5c1fe2dd7b4d6766e
Matt Patterson <matt.patterson94@gmail.com>
2014-03-24 08:13:56 +1000

    SMTP test

--- includes/send_email_quick.php
+++ includes/send_email_quick.php
@@ -87,6 +87,28 @@ function send_email($subject,$recipient,$from,$fromemail,$htmlmessage,$textmessa
 
     //Create a new PHPMailer instance
     $mail = new PHPMailer();
+    //Tell PHPMailer to use SMTP
+    $mail->isSMTP();
+    // //Enable SMTP debugging
+    // // 0 = off (for production use)
+    // // 1 = client messages
+    // // 2 = client and server messages
+    $mail->SMTPDebug = 2;
+    // //Ask for HTML-friendly debug output
+     //Set the hostname of the mail server
+    $mail->Host = 'mail.edgepcs.com.au';
+    //Set the SMTP port number - 587 for authenticated TLS, a.k.a. RFC4409 SMTP submission
+    $mail->Port = 587;
+    //Set the encryption system to use - ssl (deprecated) or tls
+    $mail->SMTPSecure = 'tls';
+    //Whether to use SMTP authentication
+    $mail->SMTPAuth = true;
+    //Username to use for SMTP authentication - use full email address for gmail
+    $mail->Username = "edge";
+    //Password to use for SMTP authentication
+    $mail->Password = "DzHeyYA94VXD6ry";
+    //Set who the message is to be sent from
+    $mail->setFrom($fromemail, $from);
     //Set an alternative reply-to address
     // $mail->addReplyTo('replyto@example.com', 'First Last');
     //Set who the message is to be sent to
